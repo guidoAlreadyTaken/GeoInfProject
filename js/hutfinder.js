ada2b9b85b693c5ec50bb15904a9d3b26bbf0549
guidoAlreadyTaken <chris.baer9105@gmail.com>
2018-01-11 12:57:52 +0100

    Fichier séparé

--- js/hutfinder.js
+++ js/hutfinder.js
@@ -1,1 +1,14 @@
-salut
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNiYWVyIiwiYSI6ImNqYjZsa2o4bjNudnoycW85b2d2ejAxamQifQ.lhJXOzm7Wl1GGyIZRUAECg';
+var map;
+
+// création de la carte
+map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-79.4512, 43.6568],
+    zoom: 13
+});
+
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+}));
